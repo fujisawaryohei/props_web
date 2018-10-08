932617353c4e1b60f351bd0839dc62e5f179873f
vagrant <vagrant@localhost.localdomain>
2018-10-08 19:10:32 +0900

    マイグレーションファイル のスキーマの変更

--- db/migrate/20181006063752_add_columns_to_users.rb
+++ db/migrate/20181006063752_add_columns_to_users.rb
@@ -1,8 +1,9 @@
 class AddColumnsToUsers < ActiveRecord::Migration[5.1]
   def change
-    add_column :users,:name,:string,null:false,index:true
-    add_column :users,:image,:string
-    add_column :users,:self_comment,:text
-    add_column :users,:admin,:boolean,null:false
+    add_column :users, :name, :string, null:false, index:true
+    add_column :users, :image, :string
+    add_column :users, :self_comment, :text
+    add_column :users, :admin, :boolean, null:false
+    add_column :users, :notified_by_id, :integer ,null:false
   end
 end
--- db/migrate/20181006181917_create_relationships.rb
+++ db/migrate/20181006181917_create_relationships.rb
@@ -1,8 +1,8 @@
 class CreateRelationships < ActiveRecord::Migration[5.1]
   def change
     create_table :relationships do |t|
-      t.references :follower, foreign_key: true,null:false,index:true
-      t.references :followed, foreign_key: true,null:false,index:true
+      t.integer :follower, foreign_key: true,null:false, index:true, unique:true
+      t.integer :followed, foreign_key: true,null:false, index:true, unique:true
 
       #t.timestamps
     end
