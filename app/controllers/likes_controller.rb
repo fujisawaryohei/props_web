dda265dd3f9e1fbebf7e7f67b8b857afb9ab7d2e
KengoAraki <kenflum@gmail.com>
2018-10-09 14:01:16 +0900

    add create action and destroy action into likes_controller

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,21 @@
 class LikesController < ApplicationController
+  before_action :set_variables
+
+  def create
+    @like = Like.create(user_id: current_user.id, post_id: params[:post_id])
+  end
+
+  def destroy
+    like = Like.find_by(user_id: current_user.id, post_id: params[:post_id])
+    like.destroy
+  end
+
+  private
+
+  def set_variables
+    #投稿
+    @post = Post.find_by(params[:post_id])
+    #like数
+    @likes_count = Like.where(post_id: params[:post_id]).count
+  end
 end
