30efaee441a1a611d5701b3f6492bfb6c0f7cef7
KengoAraki <kenflum@gmail.com>
2018-10-10 11:51:13 +0900

    clips_controllerにcreateアクションとdestroyアクションを追加

--- app/controllers/clips_controller.rb
+++ app/controllers/clips_controller.rb
@@ -1,2 +1,21 @@
 class ClipsController < ApplicationController
+  before_action :set_variables
+
+  def create
+    @clip = Clip.create(user_id: current_user.id, post_id: params[:post_id])
+  end
+
+  def destroy
+    clip = Clip.find_by(user_id: current_user.id, post_id: params[:post_id])
+    clip.destroy
+  end
+
+  private
+
+  def set_variables
+    #投稿
+    @post = Post.find_by(params[:post_id])
+    #clip数
+    @clips_count = Clip.where(post_id: params[:post_id]).count
+  end
 end
