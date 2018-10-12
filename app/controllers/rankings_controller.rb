e1fdc2b672359f7c8501b429e5abee18c155b3b1
KengoAraki <kenflum@gmail.com>
2018-10-12 13:03:12 +0900

    rakings_controllerのuser_rankingアクションの編集

--- app/controllers/rankings_controller.rb
+++ app/controllers/rankings_controller.rb
@@ -5,5 +5,9 @@ class RankingsController < ApplicationController
   end
 
   def user_ranking
+    #フォロワーの多いユーザ10人のuser_idを順に取得
+    user_rank_ids = Relationship.group(:followed).order('count_followed').limit(10).count(:followed)
+    #user_rank_idsで並んでいる順にユーザのデータを配列として消す
+    @user_ranks = user_rank_ids.map { |id| User.find(id) }
   end
 end
