570379e86b3939fdf43cbf9d30c4ad815dc71404
KengoAraki <kenflum@gmail.com>
2018-10-12 13:05:08 +0900

    userモデルのアソシエーションの訂正

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
   has_many :likes, dependent: :destroy
 
   has_many :active_relationships,
-  class_name: "Relationshp",
+  class_name: "Relationship",
   foreign_key: "follower_id",
   dependent: :destroy
 
