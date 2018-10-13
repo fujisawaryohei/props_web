bec045f038ba4f73c496f26b9b77ca143092ddca
KengoAraki <kenflum@gmail.com>
2018-10-13 15:43:08 +0900

    likes_controllerの編集

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,6 +1,4 @@
 class LikesController < ApplicationController
-  before_action :set_variables
-
   def create
     @like = Like.create(user_id: current_user.id, post_id: params[:post_id])
   end
@@ -8,14 +6,5 @@ class LikesController < ApplicationController
   def destroy
     like = Like.find_by(user_id: current_user.id, post_id: params[:post_id])
     like.destroy
-  end
-
-  private
-
-  def set_variables
-    #投稿
-    @post = Post.find_by(params[:post_id])
-    #いいね数
-    @likes_count = Like.where(post_id: params[:post_id]).count
   end
 end
