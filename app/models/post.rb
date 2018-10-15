a44e4670c01b1545f9c9b18bc8769fd2d56b230f
KengoAraki <kenflum@gmail.com>
2018-10-15 14:42:36 +0900

    postモデルのアソシエーションの修正

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_many :comments, dependent: :destroy
-  has_many :clpis, dependent: :destroy
+  has_many :clips, dependent: :destroy
   belongs_to :user
 end
