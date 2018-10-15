75764d969b197a50cf64832d210e6feda9eb47d7
KengoAraki <kenflum@gmail.com>
2018-10-16 00:09:56 +0900

    layouts/sub_layout.html.erbの追加

--- app/controllers/rankings_controller.rb
+++ app/controllers/rankings_controller.rb
@@ -1,9 +1,12 @@
 class RankingsController < ApplicationController
+  layout 'sub_layout'
   before_action :post_ranking, :user_ranking
 
   def post_ranking
+
     #いいねの数が多い投稿を順に10個product_idを取得
     post_rank_ids = Like.group(:post_id).order('count_post_id DESC').limit(10).count(:post_id).keys
+
     #post_rank_idsで並んでいる順に投稿を配列として返す
     @post_ranks = post_rank_ids.map { |id| Post.find(id) }
   end
