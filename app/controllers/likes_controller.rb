89540f96a98c70d3b9a2fcdc760ff6c23573cf3b
KengoAraki <kenflum@gmail.com>
2018-10-26 13:00:00 +0900

    likes_controllerにパラメータ検証の追加

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,10 +1,23 @@
 class LikesController < ApplicationController
   def create
-    @like = Like.create(user_id: current_user.id, post_id: params[:post_id])
+    @like = Like.new(user_id: current_user.id, post_id: params[:post_id])
+    if @like.save
+      respond_to do |format|
+        format.json
+      end
+    else
+      render nothing: true, status: 404
+    end
   end
 
   def destroy
     like = Like.find_by(user_id: current_user.id, post_id: params[:post_id])
-    like.destroy
+    if like.destroy
+      respond_to do |format|
+        format.json
+      end
+    else
+      render nothing: true, status: 404
+    end
   end
 end
