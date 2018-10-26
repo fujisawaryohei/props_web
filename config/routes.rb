24333f3b1c216d0d15b9141f0a7dc822737748a3
KengoAraki <kenflum@gmail.com>
2018-10-26 13:16:47 +0900

    routes.rbにuser、posts、likesのルーティングの削除(コンフリクト解消のため）

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,4 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'rankings/post_ranking', to: 'rankings#post_ranking'
-
-  resources :user
-  resources :posts do
-    resources :likes, only: [:create, :destroy]
-  end
 end
