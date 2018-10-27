e10a1803b9a69f288ffb36835f1508e5c0092ced
KengoAraki <kenflum@gmail.com>
2018-10-27 11:26:53 +0900

    ルーティングの削除(コンフリクト解消のため)

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :posts do
-    resources :clips, only: [:create, :destroy]
-  end
+  get 'rankings/post_ranking', to: 'rankings#post_ranking'
 end
