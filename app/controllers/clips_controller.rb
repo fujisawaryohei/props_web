027a28426077d47f873e3f3f010c5900219018df
KengoAraki <kenflum@gmail.com>
2018-10-27 11:26:20 +0900

    clips_controllerのパラメータ検証の追記

--- app/controllers/clips_controller.rb
+++ app/controllers/clips_controller.rb
@@ -1,12 +1,25 @@
 class ClipsController < ApplicationController
 
   def create
-    @clip = Clip.create(user_id: current_user.id, post_id: params[:post_id])
+    @clip = Clip.new(user_id: current_user.id, post_id: params[:post_id])
+    if @clip.save
+      respond_to do |format|
+        format.json
+      end
+    else
+      render nothing: true, status: 404
+    end
   end
 
   def destroy
     clip = Clip.find_by(user_id: current_user.id, post_id: params[:post_id])
-    clip.destroy
+    if clip.destroy
+      respond_to do |format|
+        format.json
+      end
+    else
+      render nothing: true, status: 404
+    end
   end
 
 end
