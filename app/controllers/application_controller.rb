1235d63f9e12ae1f25c845ddb6ee5e31272ce8d9
KengoAraki <kenflum@gmail.com>
2018-11-08 10:06:45 +0900

    searchアクションをapplicationコントローラに追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :search
+
+  def search
+    @q = params[:q]
+    @searched_posts = Post.search(title_cont: @q).result
+    @searched_users = User.search(name_start: @q).result
+  end
 end
