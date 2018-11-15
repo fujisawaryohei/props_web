6f58f3ca065ff4423b5cf16b42ec5bfac86c2cbc
KengoAraki <kenflum@gmail.com>
2018-11-16 01:33:48 +0900

    user_rankingのルーティングの追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'rankings/post_ranking', to: 'rankings#post_ranking'
+  get 'rankings/user_ranking', to: 'rankings#user_ranking'
 end
