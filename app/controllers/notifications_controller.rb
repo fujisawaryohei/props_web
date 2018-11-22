498a24009568b7f0b77fab9b1540e81cffeaf789
KengoAraki <kenflum@gmail.com>
2018-11-23 00:06:20 +0900

    notificationsコントローラの編集

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,4 +1,13 @@
 class NotificationsController < ApplicationController
+  layout 'sub_layout'
+
   def index
+  end
+
+  private
+
+  def set_variables
+    #notified_typeがfollowのときpost_idはnullになってしまうことに注意
+    @notifications = current_user.notifications.where(read: false).order('created_at DESC')
   end
 end
