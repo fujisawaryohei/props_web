087db3971fd5830bef7b0db997181c6f834527ad
KengoAraki <kenflum@gmail.com>
2018-11-28 08:37:47 +0900

    like_some_notificationメソッドとclip_some_notificationメソッドの一括化 passed_timeメソッドの返り値を秒から分に変更

--- app/helpers/notifications_helper.rb
+++ app/helpers/notifications_helper.rb
@@ -3,13 +3,13 @@ module NotificationsHelper
     time_ago_in_words(datetime) + '前'
   end
 
-  def passed_time(notification)
-    (Time.now - notification.post.created_at.to_time)
+  def passed_time_min(notification)
+    (Time.now - notification.post.created_at.to_time)/60
   end
 
-  def like_some_notification?(notification)
-    latest_like_some_nt = Notification.where(user_id: notification.user_id, post_id: notification.post_id, notified_type: 'like', read: false).order('created_at DESC').first
-    if latest_like_some_nt == notification
+  def some_notifications?(notification)
+    latest_some_nt = Notification.where(user_id: notification.user_id, post_id: notification.post_id, notified_type: notification.notified_type, read: false).order('created_at DESC').first
+    if latest_some_nt == notification
       return "true"
     else
       return "false"
@@ -18,15 +18,6 @@ module NotificationsHelper
 
   def some_notifications(notification)
     Notification.where(user_id: notification.user_id, post_id: notification.post_id, notified_type: notification.notified_type, read: false).order('created_at DESC')
-  end
-
-  def clip_some_notification?(notification)
-    latest_clip_some_nt = Notification.where(user_id: notification.user_id, post_id: notification.post_id, notified_type: 'clip', read: false).order('created_at DESC').first
-    if latest_clip_some_nt == notification
-      return "true"
-    else
-      return "false"
-    end
   end
 
   def clip_some_notifications(notification)
