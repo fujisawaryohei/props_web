b0a79bb94efcf63e688b68031b3fa6ccd16086c0
KengoAraki <kenflum@gmail.com>
2018-12-04 23:22:31 +0900

    @userの一括化

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,5 +1,6 @@
 class UserController < ApplicationController
   layout 'sub_layout'
+  before_action :other_user, only: [:show, :following, :follower, :like, :clip]
 
   def index
     @user = User.find_by(id: current_user)
@@ -7,7 +8,6 @@ class UserController < ApplicationController
   end
 
   def show
-    @user = User.find_by(id: params[:id])
     @posts = @user.posts.order('created_at DESC')
   end
 
@@ -18,14 +18,13 @@ class UserController < ApplicationController
   def update
     @user = User.find_by(id: current_user)
     if @user.update(user_params)
-      redirect_to user_index_path, notice: 'ユーザー情報を編集しました'
+      redirect_to user_index_path
     else
-      render :edit, alert: 'もう一度入力してください'
+      render :edit
     end
   end
 
   def following
-    @user = User.find_by(id: params[:id])
     @user_followings = @user.followings
     respond_to do |format|
       format.js
@@ -33,7 +32,6 @@ class UserController < ApplicationController
   end
 
   def follower
-    @user = User.find_by(id: params[:id])
     @user_followers = @user.followers
     respond_to do |format|
       format.js
@@ -41,7 +39,6 @@ class UserController < ApplicationController
   end
 
   def like
-    @user = User.find_by(id: params[:id])
     user_likes = @user.likes
     @posts = user_likes.map { |like| Post.find_by(id: like.post_id) }
     respond_to do |format|
@@ -50,7 +47,6 @@ class UserController < ApplicationController
   end
 
   def clip
-    @user = User.find_by(id: params[:id])
     user_clips = @user.clips
     @posts = user_clips.map { |clip| Post.find_by(id: clip.post_id) }
     respond_to do |format|
@@ -63,4 +59,9 @@ class UserController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :email, :self_comment, :image)
   end
+
+  def other_user
+    @user = User.find_by(id: params[:id])
+  end
+
 end
