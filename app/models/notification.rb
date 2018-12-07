9dd6801e7afb962bcaf67eb90f09b7d0c233b35f
KengoAraki <kenflum@gmail.com>
2018-12-07 14:04:39 +0900

    アソシエーションの修正

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,5 +1,5 @@
 class Notification < ApplicationRecord
-  belongs_to :notified_by, class_name: "User"
+  # belongs_to :notified_by, class_name: "User"
   belongs_to :user
   belongs_to :post, optional: true
 end
