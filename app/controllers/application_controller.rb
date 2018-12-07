984e92e02c4caae88d2ff40d07510fda14154f4b
KengoAraki <kenflum@gmail.com>
2018-12-08 04:28:04 +0900

    ランキング機能の修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :search
+  before_action :sidebar_ranking
 
   def search
     @q = params[:q]
@@ -17,5 +18,18 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+
+  protected
+
+  def sidebar_ranking
+    #いいねの数が多い投稿を順に10個product_idを取得
+    post_rank_ids = Like.group(:post_id).order('count_post_id DESC').limit(5).count(:post_id).keys
+    #post_rank_idsで並んでいる順に投稿を配列として返す
+    @side_post_ranks = post_rank_ids.map { |id| Post.find(id) }
+    #フォロワーの多いユーザ10人のuser_idを順に取得
+    user_rank_ids = Relationship.group(:following_id).order('count_following_id DESC').limit(5).count(:following_id).keys
+    #user_rank_idsで並んでいる順にユーザのデータを配列として消す
+    @side_user_ranks = user_rank_ids.map { |id| User.find(id) }
   end
 end
