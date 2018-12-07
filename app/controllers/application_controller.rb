b66546976ed2b9c000c006013031d12c06b02942
KengoAraki <kenflum@gmail.com>
2018-12-08 01:43:27 +0900

    新規登録のためにconfigure_permitted_parametersの設置

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :search
 
   def search
@@ -10,5 +11,11 @@ class ApplicationController < ActionController::Base
 
   def after_sign_out_path_for(resource)
     root_path
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
 end
