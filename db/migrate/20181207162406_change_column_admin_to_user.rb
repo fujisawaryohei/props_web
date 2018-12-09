cad5f5a7ba10024a583245ce1b9b22ef434b167c
KengoAraki <kenflum@gmail.com>
2018-12-09 20:19:21 +0900

    usersテーブルのadminカラムのオプションの設定

--- db/migrate/20181207162406_change_column_admin_to_user.rb
+++ db/migrate/20181207162406_change_column_admin_to_user.rb
@@ -1,10 +1,12 @@
 class ChangeColumnAdminToUser < ActiveRecord::Migration[5.1]
   def up
-        change_column :users, :admin, :boolean, null:false, default: 0
+    change_column_null :users, :admin, false, 0
+    change_column :users, :admin, :boolean, default: 0
   end
 
   # 変更前の状態
   def down
-    change_column :users, :admin, :boolean, null:false
+    change_column_null :users, :admin, true, nil
+    change_column :users, :admin, :boolean, default: nil
   end
 end
